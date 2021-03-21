580012b4f4c91e9911b5759fa2557002f57b0953
dearshrewdwit <dearshrewdwit@gmail.com>
2021-03-21 11:52:52 +0000

    setup OOD challenge

---
+++ src/bitmap-editor.js
@@ -0,0 +1,45 @@
+const os = require('os');
+
+class BitmapEditor {
+  constructor(display) {
+    this.display = display
+  }
+
+  run() {
+    process.stdout.write(`type ? for help${os.EOL}`);
+    this.display.setPrompt('> ');
+    this.display.prompt();
+
+    this.display.on('line', (line) => {
+      const command = line.trim()
+
+      if (command === '?') {
+        process.stdout.write(this.helpMessage());
+      } else if (command === 'X') {
+        this.display.close()
+      } else {
+        process.stdout.write(`unrecognised command :(${os.EOL}`);
+      }
+      this.display.prompt()
+
+    })
+
+    this.display.on('close', () => {
+      process.stdout.write(`goodbye! :)${os.EOL}`);
+      process.exit(0);
+    })
+  }
+
+  helpMessage() {
+    return `? - Help
+I M N - Create a new M x N image with all pixels coloured white (O).
+C - Clears the table, setting all pixels to white (O).
+L X Y C - Colours the pixel (X,Y) with colour C.
+V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
+H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
+S - Show the contents of the current image
+X - Terminate the session${os.EOL}`;
+  }
+}
+
+module.exports = BitmapEditor;
